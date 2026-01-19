b928172b2a2e6fa6feb558ba60f6228e72015dd2
Charith <charith@Chariths-MacBook-Pro.local>
2026-01-19 18:34:58 +0900

    Updated workflow

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,28 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+function getBase() {
+  const buildEnv = process.env.BUILD_ENV
+
+  const CDN_BASE = {
+    production: 'https://dop5fq635s7uz.cloudfront.net/', // Your CloudFront domain
+    dev: 'https://dop5fq635s7uz.cloudfront.net/',
+  }
+
+  if (buildEnv === 'production' || buildEnv === 'dev') {
+    return CDN_BASE[buildEnv]
+  }
+
+  return '/'
+}
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
+  base: getBase(),
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
